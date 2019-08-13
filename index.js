b4a043e3ff598fd44e60f87035aefe98d86167f1
Dev <Dev@8P9NXT2.in.oracle.com>
2019-08-14 00:51:42 +0530

    process.env.PORT

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 // var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 4000;
+var serverPort = normalizePort(process.env.PORT || '3000');
 
 // swaggerRouter configuration
 var options = {
